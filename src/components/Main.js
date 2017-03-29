eee881589f1faa32b0fb350164df621cec833bf4
zhaoxin <devilliu@vip.qq.com>
2017-03-29 11:23:25 +0800

    done

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,27 +12,91 @@ imageData = ((imageDataArray) => {
 	})
 	return imageDataArray;
 })(imageData);
+/*
+ *获取区间内的随机值
+ */
+let getRangeRandom = (low, high) => Math.ceil(Math.random() * (high - low) + low);
+/*
+ *随机获取0-30正负旋转角度
+ */
+let get30DegRandom = () => (Math.random() > 0.5 ? '' : '-') + Math.ceil(Math.random() * 30);
 class ImageFigure extends React.Component {
-	render() {
-		let styleObj={};
-		if(this.props.arrange.pos){
-			styleObj=this.props.arrange.pos;
-			console.log(styleObj);
+	constructor(props) {
+			super(props);
+			this.handleClick = this.handleClick.bind(this);
 		}
+		/*
+		 *imageFigure 的点击处理函数
+		 */
+	handleClick(e) {
+		if (this.props.arrange.isCenter) {
+			this.props.inverse();
+		} else {
+			this.props.center()
+		}
+		e.stopPropagation();
+		e.preventDefault();
+	}
+	render() {
+		let styleObj = {};
+		if (this.props.arrange.pos) {
+			styleObj = this.props.arrange.pos;
+		}
+		if (this.props.arrange.rotate) {
+			const rotate = this.props.arrange.rotate;
+			(['MozTransform', 'msTransform', 'WebkitTransform', 'transform']).forEach((value) => {
+				styleObj[value] = 'rotate(' + rotate + 'deg)';
+			})
+		}
+		if (this.props.arrange.isCenter) {
+			styleObj.zIndex = 11;
+		}
+		let ImageFigureClassName = 'img-figure';
+		ImageFigureClassName += this.props.arrange.isInverse ? ' is-inverse' : '';
 		return (
-			<figure className="img-figure" style={styleObj}>
-				<img src={this.props.data.url} alt={this.props.data.title}/>
-				<figcaption>
-					<h2 className="img-title">{this.props.data.title}</h2>
-				</figcaption>
-			</figure>
+			<figure className={ImageFigureClassName} style={styleObj} onClick={this.handleClick}>
+					<img src={this.props.data.url} alt={this.props.data.title}/>
+					<figcaption>
+						<h2 className="img-title">{this.props.data.title}</h2>
+						<div className="img-back" onClick={this.handleClick}>
+	                        <p>
+	                          {this.props.data.desc}
+	                        </p>
+	                    </div>
+					</figcaption>
+				</figure>
 		);
 	}
 }
-/*
-*获取区间内的随机值
-*/
-let gerRangeRandom=(low,high)=>Math.ceil(Math.random()*(high-low)+low);
+class ControllerUnits extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleClick = this.handleClick.bind(this);
+	}
+	handleClick(e) {
+		if (this.props.arrange.isCenter) {
+			this.props.inverse();
+		} else {
+			this.props.center();
+		}
+		e.preventDefault();
+		e.stopPropagation();
+	}
+	render() {
+		let controllerUnitClassName = 'controller-unit';
+		if (this.props.arrange.isCenter) {
+			controllerUnitClassName += ' is-center';
+
+			if (this.props.arrange.isInverse) {
+				controllerUnitClassName += ' is-inverse';
+			}
+		}
+		return (
+			<span className={controllerUnitClassName} onClick={this.handleClick}>
+			</span>
+		);
+	}
+}
 class AppComponent extends React.Component {
 	constructor(props) {
 			super(props);
@@ -43,7 +107,10 @@ class AppComponent extends React.Component {
 							left:'0',
 							right:'0'
 						}
-					}
+					},
+					rotate:0, //旋转角度
+					isInverse:false, //是否翻转
+					isCenter:false, //是否居中
 					*/
 				]
 			}
@@ -64,58 +131,93 @@ class AppComponent extends React.Component {
 			}
 		}
 		/*
+		 *翻转图片
+		 *@param index 输入当前被反转图片的index值
+		 *@return {Function}
+		 */
+	inverse(index) {
+			return () => {
+				let imgsArrangeArr = this.state.imgsArrangeArr;
+				imgsArrangeArr[index].isInverse = !imgsArrangeArr[index].isInverse;
+				this.setState({
+					imgsArrangeArr: imgsArrangeArr
+				})
+			}
+		}
+		/*
 		 *重新布局所有图片
 		 *@param 指定需要居中的图片
 		 */
 	rearrange(centerIndex) {
-		let imgsArrangeArr = this.state.imgsArrangeArr,
-			Constant = this.Constant,
-			centerPos = Constant.centerPos,
-			hPosRange = Constant.hPosRange,
-			vPosRange = Constant.vPosRange,
-			hPosRangeLeftSecX=hPosRange.leftSecX,
-			hPosRangeRighrSecx=hPosRange.rightSecX,
-			hPosRangeY=hPosRange.y,
-			vPosRangeTopY=vPosRange.topY,
-			vPosRangeX=vPosRange.x,
-			imgsArrangeTopArr = [],
-			topImgNum = Math.floor(Math.random()*2),
-			topImgSpliceIndex = 0,
-			imgsArrangeCenterArr = imgsArrangeArr.splice(centerIndex, 1);
-		//居中centerIndex
-		imgsArrangeCenterArr[0].pos = centerPos;
-		//取出要布局上侧的图片信息
-		topImgSpliceIndex = Math.ceil(Math.random() * (imgsArrangeArr.lengh - topImgNum));
-		imgsArrangeTopArr = imgsArrangeArr.splice(topImgSpliceIndex, topImgNum);
-		//布局上侧图片
-		imgsArrangeTopArr.forEach((value,index)=>{
-			imgsArrangeTopArr[index].pos={
-				top:gerRangeRandom(vPosRangeTopY[0],vPosRangeTopY[1]),
-				left:gerRangeRandom(vPosRangeX[0],vPosRangeX[1])
+			let imgsArrangeArr = this.state.imgsArrangeArr,
+				Constant = this.Constant,
+				centerPos = Constant.centerPos,
+				hPosRange = Constant.hPosRange,
+				vPosRange = Constant.vPosRange,
+				hPosRangeLeftSecX = hPosRange.leftSecX,
+				hPosRangeRighrSecx = hPosRange.rightSecX,
+				hPosRangeY = hPosRange.y,
+				vPosRangeTopY = vPosRange.topY,
+				vPosRangeX = vPosRange.x,
+				imgsArrangeTopArr = [],
+				topImgNum = Math.floor(Math.random() * 2),
+				topImgSpliceIndex = 0,
+				imgsArrangeCenterArr = imgsArrangeArr.splice(centerIndex, 1);
+			//居中centerIndex
+			imgsArrangeCenterArr[0] = {
+				pos: centerPos,
+				rotate: 0,
+				isCenter: true
+			};
+			//取出要布局上侧的图片信息
+			topImgSpliceIndex = Math.ceil(Math.random() * (imgsArrangeArr.lengh - topImgNum));
+			imgsArrangeTopArr = imgsArrangeArr.splice(topImgSpliceIndex, topImgNum);
+			//布局上侧图片
+			imgsArrangeTopArr.forEach((value, index) => {
+				imgsArrangeTopArr[index] = {
+					pos: {
+						top: getRangeRandom(vPosRangeTopY[0], vPosRangeTopY[1]),
+						left: getRangeRandom(vPosRangeX[0], vPosRangeX[1])
+					},
+					rotate: get30DegRandom(),
+					isCenter: false
+				}
+			});
+			//布局左右两侧图片
+			for (let i = 0, j = imgsArrangeArr.length, k = j / 2; i < j; i++) {
+				let hPosRangeLORX = null;
+				if (i < k) {
+					hPosRangeLORX = hPosRangeLeftSecX;
+				} else {
+					hPosRangeLORX = hPosRangeRighrSecx;
+				}
+				imgsArrangeArr[i] = {
+					pos: {
+						top: getRangeRandom(hPosRangeY[0], hPosRangeY[1]),
+						left: getRangeRandom(hPosRangeLORX[0], hPosRangeLORX[1])
+					},
+					rotate: get30DegRandom(),
+					isCenter: false
+				}
 			}
-		});
-		//布局左右两侧图片
-		for(let i=0,j=imgsArrangeArr.length,k=j/2;i<j;i++){
-			let hPosRangeLORX=null;
-			if(i<k){
-				hPosRangeLORX=hPosRangeLeftSecX;
-			}else{
-				hPosRangeLORX=hPosRangeRighrSecx;
+			if (imgsArrangeTopArr && imgsArrangeTopArr[0]) {
+				imgsArrangeArr.splice(topImgSpliceIndex, 0, imgsArrangeTopArr[0]);
 			}
-			imgsArrangeArr[i].pos={
-				top:gerRangeRandom(hPosRangeY[0],hPosRangeY[1]),
-				left:gerRangeRandom(hPosRangeLORX[0],hPosRangeLORX[1])
-			}
-		}
-		if(imgsArrangeTopArr&&imgsArrangeTopArr[0]){
-			imgsArrangeArr.splice(topImgSpliceIndex,0,imgsArrangeTopArr[0]);
-		}
 
-		imgsArrangeArr.splice(centerIndex,0,imgsArrangeCenterArr[0]);
+			imgsArrangeArr.splice(centerIndex, 0, imgsArrangeCenterArr[0]);
 
-		this.setState({
-			imgsArrangeArr:imgsArrangeArr
-		})
+			this.setState({
+				imgsArrangeArr: imgsArrangeArr
+			})
+		}
+		/*利用rearrange 函数，居中对应index的图片
+		 *@param index，需要居中的图片
+		 *return {Functoin}
+		 */
+	center(index) {
+		return () => {
+			this.rearrange(index);
+		}
 	}
 	componentDidMount() {
 		//定义舞台大小
@@ -157,13 +259,25 @@ class AppComponent extends React.Component {
 					pos: {
 						left: 0,
 						right: 0
-					}
+					},
+					rotate: 0,
+					isInverse: false,
+					isCenter: false
 				}
-			}
+			};
 			imgFigures.push(<ImageFigure
 				data={value}
+				key={index}
 				ref={'ImageFigure'+ index}
 				arrange={this.state.imgsArrangeArr[index]}
+				inverse={this.inverse(index)}
+				center={this.center(index)}
+				/>);
+			controllerUnits.push(<ControllerUnits
+				key={index}
+				arrange={this.state.imgsArrangeArr[index]}
+				inverse={this.inverse(index)}
+				center={this.center(index)}
 				/>);
 		});
 		return (
@@ -178,7 +292,5 @@ class AppComponent extends React.Component {
 		);
 	}
 }
-
 AppComponent.defaultProps = {};
-
 export default AppComponent;
